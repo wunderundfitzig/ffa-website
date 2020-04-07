c81fc76724c8f60c30d727ff2df8fa326e93d85a
Manuel Reich <mr@wundf.net>
2020-04-07 19:30:42 +0200

    Set up nextjs with typescript eslint and prettier

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div className='container'>
+    <Head>
+      <title>Abenteuerzentrum Berlin</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+    Hello World
+  </div>
+)
+
+export default Home
