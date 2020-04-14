b22bdf907c23b384b5bc773314854043d3427236
Manuel Reich <mr@wundf.net>
2020-04-14 13:51:32 +0200

    Add fonts

--- components/Layout/_GlobalStyles.tsx
+++ components/Layout/_GlobalStyles.tsx
@@ -1,14 +1,52 @@
 import { Global, css } from '@emotion/core'
 
+const fonts = [
+  {
+    family: 'Museo',
+    style: 'normal',
+    weight: 300,
+    fileName: 'museo300',
+  },
+  {
+    family: 'Museo',
+    style: 'normal',
+    weight: 500,
+    fileName: 'museo500r',
+  },
+  {
+    family: 'Museo',
+    style: 'normal',
+    weight: 700,
+    fileName: 'museo700',
+  },
+]
+
 const GlobalStyles = () => (
-  <Global
-    styles={css`
-      body {
-        margin: 0;
-        padding: 0;
-      }
-    `}
-  />
+  <>
+    <Global
+      styles={fonts.map(
+        (font) => css`
+            @font-face {
+              font-family: ${font.family};
+              font-style: ${font.style};
+              font-weight: ${font.weight};
+              src: local(''),
+                url('/fonts/${font.fileName}.woff2') format('woff2'),
+                url('/fonts/${font.fileName}.woff') format('woff');
+            }
+          `
+      )}
+    />
+    <Global
+      styles={css`
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: Museo, sans-serif;
+        }
+      `}
+    />
+  </>
 )
 
 export default GlobalStyles
