a5cd312dabbbe93cf12e555e561223a768ad73c9
Manuel Reich <mr@wundf.net>
2020-05-12 13:25:16 +0200

    Revert "Redirect /wp-json to backend"
    This reverts commit e6260e668e01db5fb3c4d0d36a15b4291e11bef5.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,16 @@ import Layout from 'components/Layout/Layout'
 import BlockRenderer from 'components/BlockRenderer/BlockRenderer'
 import Map from 'components/Map/Map'
 
-export default function Home(props: { blocks: WordpressBlock[] }) {
-  return (
-    <Layout>
-      <Head>
-        <title>Abenteuerzentrum Berlin</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <BlockRenderer blocks={props.blocks} />
-      <Map />
-    </Layout>
-  )
-}
+const Home = (props: { blocks: WordpressBlock[] }) => (
+  <Layout>
+    <Head>
+      <title>Abenteuerzentrum Berlin</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+    <BlockRenderer blocks={props.blocks} />
+    <Map />
+  </Layout>
+)
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const blocks = await getBlocks(['home'])
@@ -26,3 +24,5 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: { blocks }, // will be passed to the page component as props
   }
 }
+
+export default Home
