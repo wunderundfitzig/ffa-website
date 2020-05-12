e6260e668e01db5fb3c4d0d36a15b4291e11bef5
Manuel Reich <mr@wundf.net>
2020-05-12 13:01:03 +0200

    Redirect /wp-json to backend
    This is a little hack so we can set the wordpress url to this
    This links the whole wordpress 'view page' etc to the correct
    frontend…
    
    But for some reason wordpress uses the frontend url when accessing
    the api so just redirect it back to avoid bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,18 @@ import Layout from 'components/Layout/Layout'
 import BlockRenderer from 'components/BlockRenderer/BlockRenderer'
 import Map from 'components/Map/Map'
 
-const Home = (props: { blocks: WordpressBlock[] }) => (
-  <Layout>
-    <Head>
-      <title>Abenteuerzentrum Berlin</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
-    <BlockRenderer blocks={props.blocks} />
-    <Map />
-  </Layout>
-)
+export default function Home(props: { blocks: WordpressBlock[] }) {
+  return (
+    <Layout>
+      <Head>
+        <title>Abenteuerzentrum Berlin</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <BlockRenderer blocks={props.blocks} />
+      <Map />
+    </Layout>
+  )
+}
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const blocks = await getBlocks(['home'])
@@ -24,5 +26,3 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: { blocks }, // will be passed to the page component as props
   }
 }
-
-export default Home
