fe90bc7c092ff6891c1e09e72423017bc905cbfb
Manuel Reich <mr@wundf.net>
2020-08-01 11:36:42 +0200

    Style news slider to allow for overflow

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -1,26 +1,22 @@
 import { css } from '@emotion/core'
-import { useRef, useEffect, useLayoutEffect } from 'react'
+import { useRef, useLayoutEffect } from 'react'
 
 const sliderWrapperStyle = css`
-  overflow: hidden;
   position: relative;
+  overflow: hidden;
 `
 
-const _hiddenSlidesStyle = css`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-`
-
-const prevSlidesStyle = css`
-  ${_hiddenSlidesStyle};
+const sliderTrackStyle = css`
+  position: relative;
   left: -100%;
+  display: flex;
+  width: 100%;
+  min-width: 100%;
 `
 
-const nextSlidesStyle = css`
-  ${_hiddenSlidesStyle};
-  right: -100%;
+const slideStyle = css`
+  width: 100%;
+  min-width: 100%;
 `
 
 interface Props {
@@ -38,7 +34,7 @@ interface Props {
  */
 export default function Slider(props: Props) {
   const internalIndex = useRef<number>(props.index)
-  const contentRef = useRef<HTMLDivElement>(null)
+  const slideTrack = useRef<HTMLDivElement>(null)
   const touchStartPosition = useRef(null as React.Touch | null)
   const getSlide = props.children
   const previousSlide = getSlide(props.index - 1)
@@ -46,12 +42,11 @@ export default function Slider(props: Props) {
   const nextSlide = getSlide(props.index + 1)
 
   const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
-    if (contentRef.current !== null) contentRef.current.style.transition = ''
     touchStartPosition.current = e.changedTouches[0]
   }
 
   const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
-    if (!touchStartPosition.current || !contentRef.current) return
+    if (!touchStartPosition.current) return
 
     const diffX =
       (touchStartPosition.current.clientX - e.changedTouches[0].clientX) * -1
@@ -60,32 +55,31 @@ export default function Slider(props: Props) {
 
     if (Math.abs(diffX) < 20 || Math.abs(diffY) > Math.abs(diffX)) return
 
-    contentRef.current.style.transition = ''
-    contentRef.current.style.transform = `translateX(${diffX}px)`
+    setOffset(diffX)
     e.preventDefault()
   }
 
   const handleTouchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
-    if (!touchStartPosition.current || !contentRef.current) return
+    if (!touchStartPosition.current || !slideTrack.current) return
 
-    const { width } = contentRef.current.getBoundingClientRect()
     const diff =
       touchStartPosition.current.clientX - e.changedTouches[0].clientX
     const isForwardNavigation = diff > 0
-    const offset = isForwardNavigation ? width * -1 : width
-
     touchStartPosition.current = null
 
-    if (Math.abs(diff) < 100) return resetTransition({ animated: true })
+    if (Math.abs(diff) < 100) return setOffset(0, { animated: true })
 
     // cancel transition when there is no slide to go to
-    if (!previousSlide && !isForwardNavigation)
-      return resetTransition({ animated: true })
-    if (!nextSlide && isForwardNavigation)
-      return resetTransition({ animated: true })
+    if (
+      (!previousSlide && !isForwardNavigation) ||
+      (!nextSlide && isForwardNavigation)
+    ) {
+      return setOffset(0, { animated: true })
+    }
 
-    contentRef.current.style.transition = 'transform 0.5s'
-    contentRef.current.style.transform = `translateX(${offset}px)`
+    const { width } = slideTrack.current.getBoundingClientRect()
+    const offset = isForwardNavigation ? width * -1 : width
+    setOffset(offset, { animated: true })
 
     setTimeout(() => {
       if (isForwardNavigation) {
@@ -100,29 +94,24 @@ export default function Slider(props: Props) {
 
   const handleTouchCancel = () => {
     touchStartPosition.current = null
-    resetTransition()
+    setOffset(0)
   }
 
-  const resetTransition = ({ animated = false } = {}) => {
-    if (contentRef.current === null) return
+  const setOffset = (offset: string | number, { animated = false } = {}) => {
+    if (slideTrack.current === null) return
+    if (typeof offset === 'number') offset = `${offset}px`
 
-    contentRef.current.style.transition = animated ? 'transform 0.5s' : ''
-    contentRef.current.style.transform = 'translateX(0px)'
+    slideTrack.current.style.transition = animated ? 'transform 0.5s' : ''
+    slideTrack.current.style.transform = `translateX(${offset})`
   }
 
   useLayoutEffect(() => {
-    if (contentRef.current === null) return
-    if (props.index === internalIndex.current) return resetTransition()
+    if (props.index === internalIndex.current) return setOffset(0)
 
-    const offset = props.index > internalIndex.current ? 100 : -100
-
-    contentRef.current.style.transition = ''
-    contentRef.current.style.transform = `translateX(${offset}%)`
+    const offset = props.index > internalIndex.current ? '100%' : '-100%'
+    setOffset(offset)
     setTimeout(() => {
-      if (contentRef.current === null) return
-
-      contentRef.current.style.transition = 'transform 0.5s'
-      contentRef.current.style.transform = 'translateX(0%)'
+      setOffset(0, { animated: true })
       internalIndex.current = props.index
     }, 50)
   }, [props.index])
@@ -130,15 +119,20 @@ export default function Slider(props: Props) {
   return (
     <div
       css={sliderWrapperStyle}
+      className={props.className}
       onTouchStart={handleTouchStart}
       onTouchMove={handleTouchMove}
       onTouchEnd={handleTouchEnd}
       onTouchCancel={handleTouchCancel}
     >
-      <div ref={contentRef}>
-        <div css={prevSlidesStyle}>{previousSlide}</div>
-        {currentSlide}
-        <div css={nextSlidesStyle}>{nextSlide}</div>
+      <div css={sliderTrackStyle} ref={slideTrack}>
+        <div css={slideStyle} aria-hidden='true'>
+          {previousSlide}
+        </div>
+        <div css={slideStyle}>{currentSlide}</div>
+        <div css={slideStyle} aria-hidden='true'>
+          {nextSlide}
+        </div>
       </div>
     </div>
   )
