5a0f04c3b336b4f35f1eae4e953c1d99a5bcddd5
Manuel Reich <mr@wundf.net>
2020-08-04 09:47:41 +0200

    Show less in mobile slider

--- style/breakpoints.ts
+++ style/breakpoints.ts
@@ -6,6 +6,8 @@ export const breakpointL = 750
 export const breakpointXL = 1040
 export const breakpointXXL = 1200
 
+const orderedBreakpoints = ['xs', 's', 'm', 'l', 'xl', 'xxl']
+
 export function getBreakpoint(windowWidth: number): Breakpoint {
   if (windowWidth >= breakpointXXL) return 'xxl'
   if (windowWidth >= breakpointXL) return 'xl'
@@ -13,4 +15,19 @@ export function getBreakpoint(windowWidth: number): Breakpoint {
   if (windowWidth >= breakpointM) return 'm'
   if (windowWidth >= breakpointS) return 's'
   return 'xs'
+}
+
+export function isAtOrAboveBreakpoint(
+  breakpoint: Breakpoint,
+  windowWidth: number
+) {
+  const currentBreakpoint = getBreakpoint(windowWidth)
+  const currentBreakpointIndex = orderedBreakpoints.indexOf(currentBreakpoint)
+  const breakpointIndex = orderedBreakpoints.indexOf(breakpoint)
+
+  return currentBreakpointIndex >= breakpointIndex
+}
+
+export function isBelowBreakpoint(breakpoint: Breakpoint, windowWidth: number) {
+  return !isAtOrAboveBreakpoint(breakpoint, windowWidth)
 }
