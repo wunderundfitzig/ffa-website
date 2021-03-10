7899b1f3b44c39f8032434c89373081378099857
Manuel Reich <mr@wundf.net>
2021-03-10 23:43:57 +0100

    Add date and title image to post

--- pages/blog/posts/[post].tsx
+++ pages/blog/posts/[post].tsx
@@ -1,22 +1,37 @@
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
-import { getBlocks, WordpressPost } from 'lib/wordpressApi'
+import { getBlocks, WordpressPage } from 'lib/wordpressApi'
 import BlockRenderer from 'components/BlockRenderer/BlockRenderer'
 import Title from 'components/Title/Title'
 import { TitleBlock } from 'lib/models/titleBlock'
+import WideImage from 'components/WideImage/WideImage'
+import { colors } from 'style'
 
 interface TitleBlockWrapper {
   blockName: 'lazyblock/title'
   attrs: TitleBlock
 }
 
-export default function Page(props: WordpressPost) {
+export default function Page(props: WordpressPage) {
+  const date = new Date(props.date)
   return (
     <>
       <Head>
         <title>Abenteuerzentrum Berlin | {props.title} </title>
       </Head>
-      <Title primary roofline='Post' title={props.title} />
+      {props.image && (
+        <WideImage
+          asHeader={false}
+          image={props.image}
+          color={colors.lightGreen}
+          text={undefined}
+        />
+      )}
+      <Title
+        primary
+        roofline={date.toLocaleDateString('de')}
+        title={props.title}
+      />
       <BlockRenderer blocks={props.blocks} />
     </>
   )
